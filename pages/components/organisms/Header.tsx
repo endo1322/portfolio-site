ff89ac899893a8e89c6614e86b3b33f3aa341696
endo1322 <endo.yuto.19@shizuoka.ac.jp>
2023-01-31 14:39:40 +0900

    adjust layout

--- pages/components/organisms/Header.tsx
+++ pages/components/organisms/Header.tsx
@@ -4,9 +4,11 @@ import { Nav } from '../molecules/Nav'
 
 export const Header = () => {
   return (
-    <header className="flex max-w-7xl w-11/12 mx-auto justify-between items-center text-3xl bg-slate-400">
-      <Logo className="text-6xl" />
-      <Nav />
+    <header className=" bg-slate-400">
+      <div className="flex max-w-7xl w-11/12 mx-auto justify-between items-center text-3xl">
+        <Logo className="text-6xl" />
+        <Nav />
+      </div>
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '24px'
+      }
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
