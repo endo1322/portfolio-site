b8b04f1c6b325c3bf3e918543c85558453866019
endo1322 <bondroid3.22@icloud.com>
2023-12-04 21:23:27 +0900

    fix thumbnail layout

--- src/components/molecules/BlogCard.tsx
+++ src/components/molecules/BlogCard.tsx
@@ -18,34 +18,39 @@ export const BlogCard = (props: BlogCardProps) => {
   return (
     <div className="max-w-4xl mx-auto my-3 mb-5 p-2  bg-white  hover:bg-neutral-300 rounded-lg h-fit relative">
       <Link href={blogUrl} className="inset-0 absolute" />
-      {props.coverUrl && (
-        <Image
-          src={props.coverUrl}
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-      )}
-      <div className="pl-2">{props.createdDate}</div>
-      <div className="pl-4">
-        <div className="text-3xl mb-1">{props.title}</div>
-        <ul className="flex flex-row gap-1 flex-wrap">
-          {Object.keys(props.multiSelect).map((key) => (
-            <button
-              key={`${props.id}${key}`}
-              onClick={() => {
-                props.onSetBool({ selectedTagId: key })
-              }}
-            >
-              <Tag
-                className=""
-                id={key}
-                name={props.multiSelect[key].name}
-                color={props.multiSelect[key].color}
-              ></Tag>
-            </button>
-          ))}
-        </ul>
+      <div className="flex flex-row">
+        {props.coverUrl && (
+          <Image
+            className="rounded-md"
+            src={props.coverUrl}
+            width={200}
+            height={100}
+            alt={`Picture of ${props.title}`}
+          />
+        )}
+        <div>
+          <div className="pl-2">{props.createdDate}</div>
+          <div className="pl-4">
+            <div className="text-3xl mb-1">{props.title}</div>
+            <ul className="flex flex-row gap-1 flex-wrap">
+              {Object.keys(props.multiSelect).map((key) => (
+                <button
+                  key={`${props.id}${key}`}
+                  onClick={() => {
+                    props.onSetBool({ selectedTagId: key })
+                  }}
+                >
+                  <Tag
+                    className=""
+                    id={key}
+                    name={props.multiSelect[key].name}
+                    color={props.multiSelect[key].color}
+                  ></Tag>
+                </button>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
