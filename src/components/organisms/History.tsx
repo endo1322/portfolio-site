c5c75387ada7522d186ec596f71a362d5616636c
endo1322 <bondroid3.22@icloud.com>
2023-12-08 13:56:39 +0900

    refactor History component

--- src/components/organisms/History.tsx
+++ src/components/organisms/History.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import { Sircle } from '../atoms/Sircle'
+import { Frame } from '../atoms/Frame'
 
 type HistoryPropsType = {
   className: string
@@ -21,36 +23,37 @@ export const History = (props: HistoryPropsType) => {
             <div className="backdrop-blur flex-grow">
               {/* ここから要素 */}
               <div className="flex flex-row h-full relative">
-                <div className="flex sticky top-[7.5rem] z-10 h-max w-1/5 justify-center mt-10">
-                  <div className="flex items-center rounded-full w-40 h-40 bg-neutral-400 bg-opacity-70 shadow-lg">
-                    <div className=" text-3xl text-slate-50 font-bold mx-auto">
-                      {value.year}
-                    </div>
+                <div className="w-1/5">
+                  <div className="flex sticky top-[7.5rem] h-max w-full">
+                    <Sircle
+                      clsssName="w-40 h-40 mt-10 mx-auto bg-neutral-400 bg-opacity-70 shadow-lg"
+                      item={value.year}
+                      itemClassName={'text-3xl text-slate-50 font-bold'}
+                    />
                   </div>
                 </div>
 
-                <div className="w-3/5 my-auto sticky top-[7.5rem]">
-                  <div className=" bg-neutral-400 bg-opacity-50 text-center w-fit py-10 px-5 mx-auto rounded-lg shadow-lg">
-                    {value.text.map((item, index) => (
-                      <div
-                        className="text-xl font-bold mb-1 text-slate-50"
-                        key={index}
-                      >
-                        {item}
-                      </div>
-                    ))}
-                  </div>
+                <div className="w-3/5 sticky top-[7.5rem] my-auto">
+                  <Frame className="bg-neutral-400 bg-opacity-50 w-fit mx-auto rounded-lg shadow-lg">
+                    <div className=" text-center py-10 px-5">
+                      {value.text.map((item, index) => (
+                        <div
+                          className="text-xl font-bold mb-1 text-slate-50"
+                          key={index}
+                        >
+                          {item}
+                        </div>
+                      ))}
+                    </div>
+                  </Frame>
                 </div>
                 <div className="absolute right-0 bottom-0">
-                  <div className="text-right mb-2 mr-2">
-                    <p className=" text-neutral-400">
-                      <Link href={'https://dreamstudio.ai/'}>
-                        {
-                          '@The background image was created using StableDiffusion.'
-                        }
-                      </Link>
-                    </p>
-                  </div>
+                  <Link
+                    href={'https://dreamstudio.ai/'}
+                    className="text-right mb-2 mr-2 text-neutral-400"
+                  >
+                    {'@The background image was created using StableDiffusion.'}
+                  </Link>
                 </div>
               </div>
               {/* ここまで要素 */}
@@ -59,7 +62,7 @@ export const History = (props: HistoryPropsType) => {
         ))}
       </div>
 
-      <div className="flex">
+      {/* <div className="flex">
         <div className="flex w-5xl mx-auto relative">
           <div className="flex flex-col gap-5">
             {props.history.map((value, index) => (
@@ -84,7 +87,7 @@ export const History = (props: HistoryPropsType) => {
             <div className=" absolute h-full w-2.5 left-[4.725rem] bg-slate-700"></div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
